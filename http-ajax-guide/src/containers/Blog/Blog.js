ea4c313384ebdb2d4fb7daf434acf07206cda3ee
Yang Wei Hao <whyang0808@gmail.com>
2019-07-15 00:01:22 +0800

    Setting Up and Rendering Routes

--- http-ajax-guide/src/containers/Blog/Blog.js
+++ http-ajax-guide/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -18,7 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} />
             </div>
         );
     }

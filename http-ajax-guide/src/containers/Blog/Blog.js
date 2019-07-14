6b854719b8befd6b08bdab8c6bb5cd356b272d22
Yang Wei Hao <whyang0808@gmail.com>
2019-07-15 00:06:33 +0800

    Switching Between Pages

--- http-ajax-guide/src/containers/Blog/Blog.js
+++ http-ajax-guide/src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -21,6 +22,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
